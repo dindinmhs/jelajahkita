0699ac0711ff160589abb05ab6585b80a6f06508
dindinmhs <dindin.imanudin@mhs.itenas.ac.id>
2025-11-16 09:40:24 +0700

    no lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,17 @@
-const isProd = process.env.NODE_ENV === "production";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: isProd
-      ? {
-          "no-unused-vars": "off",
-          "no-undef": "off",
-        }
-      : {},
-  },
 ];
+
 
 export default eslintConfig;
